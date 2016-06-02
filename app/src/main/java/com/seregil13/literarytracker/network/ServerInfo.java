2e45b1e6bed2748a0556b1dbc326f7431e4e505d
seregil13 <alecerietman@gmail.com>
2016-06-01 19:35:23 -0700

    puts actual ServerInfo.java instead of symbolic link

--- app/src/main/java/com/seregil13/literarytracker/network/ServerInfo.java
+++ app/src/main/java/com/seregil13/literarytracker/network/ServerInfo.java
@@ -1,1 +1,38 @@
-/home/alec/development/configurationfiles/LiteraryTrackerAndroid/ServerInfo.java
+package com.seregil13.literarytracker.network;
+
+import com.seregil13.literarytracker.BuildConfig;
+
+import java.util.Locale;
+
+public enum ServerInfo {
+    LIGHT_NOVEL("lightnovels"),
+    MANGA("manga"),
+    BOOKS("books"),
+    GENRE("genre");
+
+    String mSubDir;
+
+    ServerInfo(String subdir) {
+        this.mSubDir = subdir;
+    }
+
+    public String getListUrl() {
+        return String.format(Locale.US, "%s/%s/list/", BuildConfig.BASE_URL, this.mSubDir);
+    }
+
+    public String getDetailUrl(int id) {
+        return String.format(Locale.US, "%s/%s/get/%d", BuildConfig.BASE_URL, this.mSubDir, id);
+    }
+
+    public String getCreateUrl() {
+        return String.format(Locale.US, "%s/%s/create", BuildConfig.BASE_URL, this.mSubDir);
+    }
+
+    public String getUpdateUrl(int id) {
+        return String.format(Locale.US, "%s/%s/update/%d", BuildConfig.BASE_URL, this.mSubDir, id);
+    }
+
+    public String getExistsUrl(String title) {
+        return String.format(Locale.US, "%s/%s/exists/%s", BuildConfig.BASE_URL, this.mSubDir, title);
+    }
+}
